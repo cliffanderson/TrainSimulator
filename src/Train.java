1b1a3c934dd35f52db74f603430b633036ddc6cf
Thomas Plano <planot@wit.edu>
2016-02-10 09:49:30 -0500

    third commit

--- src/Train.java
+++ src/Train.java
@@ -1,4 +1,14 @@
 
-public class Train {
 
+
+public class Train {
+	public Train(){
+		//generate empty bag of passengers of size max capacity 
+	}
+	public void getLocation(){
+		//return station ID
+	}
+// location 
+// max capacity
+// bag of passengers 
 }
--- src/TrainRoute.java
+++ src/TrainRoute.java
@@ -1,4 +1,18 @@
 
+import java.util.LinkedList;
+
 public class TrainRoute {
-//
+	public TrainRoute(int NumofStations){
+		// generate a list of stations 
+		list =new LinkedList<Station>();
+		
+		for (int i=0;i<NumofStations;i++){
+			Station station = new Station(i);
+			list.push(station);
+		}
+	}
+	public LinkedList<Station> getRoute(){
+		return list;
+	}
+private LinkedList<Station> list;
 }
