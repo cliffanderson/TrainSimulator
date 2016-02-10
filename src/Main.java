ba51d07d31bcd309abecbec21194720385d8a695
Thomas Plano <planot@wit.edu>
2016-02-10 08:43:01 -0500

    second commit

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		// generate train route, which generates stations 
+		// place trains at random stations
+		// generate people to fill stations
+		
+		
+		//here: board trains with people from que
+		// move trains to next station
+		// disenbark people who arrived at destination
+		// fill train with people from que
+		// goto here:
+		
+	}
+
+}
